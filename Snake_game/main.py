82c2fe8e34ba5affbb2a2a149aad10d2665648b8
Akira <isaacsamuel2003@gmail.com>
2024-09-14 13:54:48 +0530

    Fixed Scoreboard error

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,6 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
-import food
+from food import Food
 from time import sleep
 from scoreboard import Scoreboard
 
@@ -10,9 +10,10 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 cobra = Snake(6, 1)
+print("Score Incremented")
 cobra.create_snake()
 
-khana = food.Food()
+khana = Food()
 
 screen.listen()
 screen.onkey(key="Up", fun=cobra.move_up)
@@ -23,6 +24,7 @@ screen.update()
 
 
 level = Scoreboard()
+game_started = False
 
 while True:
 
@@ -30,6 +32,7 @@ while True:
         level.score_reset()
         cobra.snake_reset()
         sleep(2)
+        game_started=False
         # game = screen.textinput(title="Snake",
         #                         prompt="Do you wish to continue? Press Y to continue, and any other key to Quit")
         # if game == "Y" or game == "y":
@@ -39,16 +42,22 @@ while True:
 
     elif khana.distance(cobra.head) < 15:
         khana.respawn()
-        cobra.eat(1)
-        level.increase_score()
+        if(game_started):
+            cobra.eat(1)
+            print("Score Incremented")
+            level.increase_score()
+
 
     else:
         for i in cobra.segments[1:]:
             if cobra.head.distance(i) < 10:
                 level.score_reset()
                 cobra.snake_reset()
+                game_started=False
                 sleep(2)
+
 
         cobra.move()
         screen.update()
         sleep(0.09)
+    game_started=True
--- Snake_game/scoreboard.py
+++ Snake_game/scoreboard.py
@@ -5,13 +5,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open(file="high_score.txt", mode="r") as f:
+            self.high_score = int(f.read())
         self.teleport(x=0, y=270)
         self.hideturtle()
         self.penup()
         self.color("white")
-        self.write(arg=f"Score = {self.score}   High Score = {self.high_score}",
-                   align="center", font=("Arial", 16, "normal"))
+        self.update_scoreboard()
+
 
     def update_scoreboard(self):
         self.clear()
@@ -22,9 +23,12 @@ class Scoreboard(Turtle):
         self.clear()
         if self.score > self.high_score:
             self.high_score = self.score
+        with open(file="high_score.txt", mode="w") as f:
+            f.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
